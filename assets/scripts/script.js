971cca897abb50c3fa1381325fa71d7fd5bd7c9e
Shivam Amin <xShivam.Amin@gmail.com>
2018-01-21 04:51:43 -0500

    Fixed action buttons
    Working as intended.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,150 +1,199 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      xmldata = this.responseXML;
-      clean(xmldata);
-      createTable(xmldata);
-    }
+  if (this.readyState == 4 && this.status == 200) {
+    xmldata = this.responseXML;
+    clean(xmldata);
+    refreshTable();
+  }
 };
 xhttp.open("GET", "assets/data/data.xml", true);
 xhttp.send();
 
-function createTable() {
+function refreshTable() {
   var dvdList = xmldata.childNodes[1].childNodes;
   //Create Table and Headings
-  var body = document.getElementsByTagName("body")[0];
+  var body = document.getElementById("first");
   if (document.getElementById("DVDList")){
     document.getElementById("DVDList").remove();
   }
   var table = document.createElement("table");
   table.id = "DVDList";
+  var row = document.createElement("tr");
+  row.id = "Headers";
+  var heading = document.createElement("th");
+  var text = document.createTextNode("Title");
+  heading.style.width = "150px";
+  heading.appendChild(text);
+  row.appendChild(heading);
+  var heading = document.createElement("th");
+  var text = document.createTextNode("Description");
+  heading.appendChild(text);
+  row.appendChild(heading);
+  var heading = document.createElement("th");
+  heading.style.width = "10%";
+  var text = document.createTextNode("Price");
+  heading.appendChild(text);
+  row.appendChild(heading);
+  var heading = document.createElement("th");
+  heading.style.width = "200px";
+  var text = document.createTextNode("Genre");
+  heading.appendChild(text);
+  row.appendChild(heading);
+  var heading = document.createElement("th");
+  heading.style.width = "100px";
+  var text = document.createTextNode("Action");
+  heading.appendChild(text);
+  row.appendChild(heading);
+  table.appendChild(row);
+  //Read and Create Rows from XML
+  for(var x = 0; x < dvdList.length; x++){
     var row = document.createElement("tr");
-    row.id = "Headers";
-    var heading = document.createElement("th");
-    var text = document.createTextNode("Title");
-    heading.style.width = "150px";
-    heading.appendChild(text);
-    row.appendChild(heading);
-    var heading = document.createElement("th");
-    var text = document.createTextNode("Description");
-    heading.appendChild(text);
-    row.appendChild(heading);
-    var heading = document.createElement("th");
-    heading.style.width = "10%";
-    var text = document.createTextNode("Price");
-    heading.appendChild(text);
-    row.appendChild(heading);
-    var heading = document.createElement("th");
-    heading.style.width = "200px";
-    var text = document.createTextNode("Genre");
-    heading.appendChild(text);
-    row.appendChild(heading);
-    var heading = document.createElement("th");
-    heading.style.width = "100px";
-    var text = document.createTextNode("Action");
-    heading.appendChild(text);
-    row.appendChild(heading);
-    table.appendChild(row);
-    //Read and Create Rows from XML
-    for(var x = 0; x < dvdList.length; x++){
-      if(dvdList[x].nodeName != '#text'){
-        var row = document.createElement("tr");
-        //Inside each DVD
-        for(var y = 0; y < dvdList[x].childNodes.length; y++){
-          var column = document.createElement("td");
-          column.id = dvdList[x].childNodes[y].nodeName;
-          if(dvdList[x].childNodes[y].nodeName == 'genre'){
-            var innerTable = document.createElement("table");
-            for(var z = 0; z < dvdList[x].childNodes[y].childNodes.length; z++){
-              var innerRow = document.createElement("tr");
-              var innerColumn = document.createElement("td");
-              var innerText = document.createTextNode(dvdList[x].childNodes[y].childNodes[z].firstChild.nodeValue);
-              innerColumn.appendChild(innerText);
-              innerRow.appendChild(innerColumn);
-              innerTable.appendChild(innerRow);
-            }
-            column.appendChild(innerTable);
-          } else {
-            var text = document.createTextNode(dvdList[x].childNodes[y].firstChild.nodeValue);
-            column.appendChild(text);
-          }
-          row.appendChild(column);
+    //Inside each DVD
+    for(var y = 0; y < dvdList[x].childNodes.length; y++){
+      var column = document.createElement("td");
+      column.id = dvdList[x].childNodes[y].nodeName;
+      var tDiv = document.createElement("div");
+      var iDiv = document.createElement("div");
+      tDiv.id = "text";
+      iDiv.id = "input";
+      iDiv.style.display = 'none';
+      var input = document.createElement("textarea");
+      input.style.width = "100%";
+      input.style.resize = "none";
+      input.className = "form-control";
+      if(dvdList[x].childNodes[y].nodeName == 'genre'){
+        var innerTable = document.createElement("table")
+        for(var z = 0; z < dvdList[x].childNodes[y].childNodes.length; z++){
+          var innerRow = document.createElement("tr");
+          var innerColumn = document.createElement("td");
+          var innerText = document.createTextNode(dvdList[x].childNodes[y].childNodes[z].firstChild.nodeValue + "\n");
+          innerColumn.appendChild(innerText);
+          innerRow.appendChild(innerColumn);
+          innerTable.appendChild(innerRow);
+          //input.value += dvdList[x].childNodes[y].childNodes[z].firstChild.nodeValue + "\n"
         }
-        var col = document.createElement("td");
-        col.id = "actions";
-        var updButton = document.createElement("input");
-        updButton.type = "button";
-        updButton.className = "btn btn-default";
-        updButton.dataset.type = "Edit"
-        updButton.value = updButton.dataset.type;
-        updButton.addEventListener("click", function() {
-          if(this.dataset.type.toUpperCase() == "EDIT") {
-            var targetElem = this.parentElement.parentElement;
-            console.log(targetElem);
-            var targetXObj = parseAsXML(targetElem);
-            var xmlElement = findElementInXML(targetXObj);
-            var undoButton = document.createElement("input");
-            undoButton.type = "button";
-            undoButton.className = "btn btn-warning";
-            undoButton.value = "Reset";
-            undoButton.addEventListener("click", function() {
-              this.parentElement.firstChild.dataset.type = "Edit";
-              this.parentElement.firstChild.className = "btn btn-warning"
-              this.parentElement.firstChild.value = this.parentElement.firstChild.dataset.type;
-              this.remove();
-            });
-            this.parentElement.insertBefore(undoButton, this.nextSibling);
-            var name = targetElem.children[0];
-            var desc = targetElem.children[1];
-            var price = targetElem.children[2];
-            var genre = targetElem.children[3];
-            for(var x = 0; x < targetElem.children.length - 1; x++) {
-              var input = document.createElement("textarea");
-              input.style.width = "100%";
-              input.style.resize = "none";
-              input.className = "form-control";
-              if(targetElem.children[x].id.toUpperCase() == "GENRE") {
-                genres = new DOMParser().parseFromString(targetElem.children[x].innerHTML, "text/xml");
-                for(var y = 0; y < genres.firstChild.childNodes.length; y++) {
-                  input.value += genres.firstChild.childNodes[y].firstChild.firstChild.nodeValue + "\n";
-                }
-              } else {
-                input.value = targetElem.children[x].innerHTML;
-              }
-              targetElem.children[x].innerHTML = "";
-              targetElem.children[x].appendChild(input);
-              this.dataset.type = "Submit";
-              this.className = "btn btn-success"
-            }
-          } else if (this.dataset.type.toUpperCase() == "SUBMIT") {
-            this.nextSibling.remove();
-            this.dataset.type = "Edit";
-            this.className = "btn btn-warning"
-          }
-          this.value = this.dataset.type;
-        })
-        var delButton = document.createElement("input");
-        delButton.type = "button";
-        delButton.className = "btn btn-danger";
-        delButton.value = "Delete";
-        delButton.addEventListener("click", function(){
-          //Match and delete data from XMLObject
-          var targetElem = this.parentElement.parentElement;
-          var targetXObj = parseAsXML(targetElem);
-          findElementInXML(targetXObj).remove();
-          createTable();
-        });
-        col.appendChild(updButton);
-        col.appendChild(delButton);
-        row.appendChild(col);
+        tDiv.appendChild(innerTable);
+        column.appendChild(tDiv);
+      } else {
+        var text = document.createTextNode(dvdList[x].childNodes[y].firstChild.nodeValue);
+        tDiv.appendChild(text);
+        //input.value = text.nodeValue;
+        column.appendChild(tDiv);
       }
-      table.appendChild(row);
+      iDiv.appendChild(input);
+      column.appendChild(iDiv);
+      row.appendChild(column);
+      row.dataset.view = "text";
     }
-    //Write Table to DOM
-    body.appendChild(table);
+    var col = document.createElement("td");
+    col.id = "actions";
+    var updButton = document.createElement("input");
+    updButton.type = "button";
+    updButton.className = "btn btn-default";
+    updButton.dataset.type = "Edit"
+    updButton.value = updButton.dataset.type;
+    updButton.addEventListener("click", function() {
+      var delButton = this.nextSibling;
+      var targetElem = this.parentElement.parentElement;
+      var targetXObj = parseAsXML(targetElem);
+      var xmlElement = findElementInXML(targetXObj, false);
+      var view = targetElem.dataset.view;
+      if(view == "text") {
+        targetElem.dataset.view = "input";
+        this.value = "Submit";
+        this.className = "btn btn-success";
+        this.nextSibling.dataset.type = "Reset";
+        this.nextSibling.value = "Reset";
+        for(x = 0; x < targetElem.children.length - 1; x++) {
+          var e = targetElem.children[x];
+          inp = e.firstChild.nextSibling.firstChild;
+          e.firstChild.style.display = 'none';
+          e.firstChild.nextSibling.style.display = '';
+          if(e.id.toUpperCase() == "GENRE") {
+            genres = new DOMParser().parseFromString(e.firstChild.innerHTML, "text/xml");
+            inp.value = '';
+            for(var y = 0; y < genres.firstChild.childNodes.length; y++) {
+              inp.value += genres.firstChild.childNodes[y].firstChild.firstChild.nodeValue.trim() + "\n";
+            }
+          } else {
+            e.firstChild.nextSibling.firstChild.value = e.firstChild.innerHTML;
+          }
+        }
+      } else {
+        targetElem.dataset.view = "text";
+        this.value = "Edit";
+        this.className = "btn btn-default";
+        this.nextSibling.dataset.type = "Delete";
+        this.nextSibling.value = "Delete";
+        var dvd = document.createElement("dvd");
+        for(x = 0; x < targetElem.children.length - 1; x++) {
+          var e = targetElem.children[x];
+          inp = e.firstChild.nextSibling.firstChild;
+          e.firstChild.style.display = '';
+          e.firstChild.nextSibling.style.display = 'none';
+          var tag = document.createElement(e.id);
+          if(e.id.toUpperCase() == "GENRE") {
+            genres = new DOMParser().parseFromString(e.firstChild.innerHTML, "text/xml");
+            i = inp.value.split("\n");
+            etable = document.createElement("table");
+            for(var y = 0; y < i.length; y++) {
+              if (i[y] != "") {
+                etr = document.createElement("tr");
+                etd = document.createElement("td");
+                etxt = document.createTextNode(i[y].trim());
+                var type = document.createElement("type");
+                type.appendChild(document.createTextNode(i[y].trim()));
+                tag.appendChild(type);
+                etd.appendChild(etxt);
+                etr.appendChild(etd);
+                etable.appendChild(etr);
+              }
+            }
+            e.firstChild.replaceChild(etable, e.firstChild.firstChild);
+          } else {
+            tag.appendChild(document.createTextNode(e.firstChild.nextSibling.firstChild.value));
+            e.firstChild.innerHTML = e.firstChild.nextSibling.firstChild.value;
+          }
+          dvd.appendChild(tag);
+        }
+        xmldata.childNodes[1].replaceChild(dvd, xmlElement);
+      }
+    });
+    var delButton = document.createElement("input");
+    delButton.type = "button";
+    delButton.className = "btn btn-danger";
+    delButton.value = "Delete";
+    delButton.dataset.type = "Delete";
+    delButton.addEventListener("click", function(){
+      var targetElem = this.parentElement.parentElement;
+      var targetXObj = parseAsXML(targetElem);
+      if(this.dataset.type.toUpperCase() == 'DELETE') {
+        //Match and delete data from XMLObject
+        findElementInXML(targetXObj, false).remove();
+      } else {
+        this.dataset.type = "Delete";
+        this.previousSibling.value = "Edit";
+        targetElem.dataset.view = "text";
+        this.previousSibling.className = "btn btn-default";
+        for(x = 0; x < targetElem.children.length - 1; x++) {
+          var e = targetElem.children[x];
+          e.firstChild.style.display = '';
+          e.firstChild.nextSibling.style.display = 'none';
+        }
+      }
+      this.value = this.dataset.type;
+      refreshTable();
+    });
+    col.appendChild(updButton);
+    col.appendChild(delButton);
+    row.appendChild(col);
+    table.appendChild(row);
+  }
+  //Write Table to DOM
+  body.appendChild(table);
 }
 
-function findElementInXML(targetXObj) {
+function findElementInXML(targetXObj, bool) {
   var listOfElem = xmldata.childNodes[1].childNodes;
   for(var x = 0; x < listOfElem.length; x++) {
     var name = targetXObj.childNodes[0].firstChild.nodeValue;
@@ -152,13 +201,17 @@ function findElementInXML(targetXObj) {
     var price = targetXObj.childNodes[2].firstChild.nodeValue;
     var genres = [];
     for(var y = 0; y < targetXObj.childNodes[3].childNodes.length; y++){
-      genres.push(targetXObj.childNodes[3].childNodes[y].firstChild.nodeValue)
+      genres.push(targetXObj.childNodes[3].childNodes[y].firstChild.nodeValue.trim());
     }
     if(name == listOfElem[x].childNodes[0].firstChild.nodeValue && desc == listOfElem[x].childNodes[1].firstChild.nodeValue && price == listOfElem[x].childNodes[2].firstChild.nodeValue && genres.length == listOfElem[x].childNodes[3].childNodes.length){
       for (var z = 0; z < genres.length; z++) {
         if(genres[z] == listOfElem[x].childNodes[3].childNodes[z].firstChild.nodeValue) {
           if(z == genres.length - 1) {
-            return listOfElem[x];
+            if(bool) {
+              return x;
+            } else {
+              return listOfElem[x];
+            }
           }
           continue;
         } else {
@@ -172,13 +225,13 @@ function findElementInXML(targetXObj) {
 function parseAsXML(elem) {
   var xmlDVD = document.createElement("DVD");
   var xmlName = document.createElement("Name");
-  xmlName.appendChild(document.createTextNode(elem.childNodes[0].firstChild.nodeValue));
+  xmlName.appendChild(document.createTextNode(elem.childNodes[0].firstChild.firstChild.nodeValue));
   var xmlDesc = document.createElement("Desc");
-  xmlDesc.appendChild(document.createTextNode(elem.childNodes[1].firstChild.nodeValue));
+  xmlDesc.appendChild(document.createTextNode(elem.childNodes[1].firstChild.firstChild.nodeValue));
   var xmlPrice = document.createElement("Price");
-  xmlPrice.appendChild(document.createTextNode(elem.childNodes[2].firstChild.nodeValue));
+  xmlPrice.appendChild(document.createTextNode(elem.childNodes[2].firstChild.firstChild.nodeValue));
   var xmlGenre = document.createElement("Genre");
-  var genres = elem.childNodes[3].firstChild.childNodes;
+  var genres = elem.childNodes[3].firstChild.firstChild.childNodes;
   for (var x = 0; x < genres.length; x++) {
     var xmlType = document.createElement("Type");
     xmlType.appendChild(document.createTextNode(genres[x].firstChild.firstChild.nodeValue));
@@ -193,10 +246,10 @@ function parseAsXML(elem) {
 
 function insertDVD() {
   //Get data from form
-  var name = document.getElementById("name").value;
-  var desc = document.getElementById("desc").value;
-  var price = document.getElementById("price").value;
-  var genrelist = document.getElementById("genre").options;
+  var name = document.getElementById("addName").value;
+  var desc = document.getElementById("addDesc").value;
+  var price = document.getElementById("addPrice").value;
+  var genrelist = document.getElementById("addGenre").options;
   var genres = [];
   for(var x = 0; x < genrelist.length; x++) {
     if (genrelist[x].selected) {
@@ -239,7 +292,7 @@ function insertDVD() {
           xmlDVD.appendChild(xmlGenre);
           //insert new xml element into xmlObject
           xmldata.childNodes[1].appendChild(xmlDVD);
-          createTable();
+          refreshTable();
         }
       }
     }
